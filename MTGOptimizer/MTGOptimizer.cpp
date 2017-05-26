74bfacccac5ae19bc610a5533173c140cea42b1c
Hugh Williams <hughaw64@gmail.com>
2017-05-26 19:54:49 +0100

    Changed current test deck to include different CMC's

--- MTGOptimizer/MTGOptimizer.cpp
+++ MTGOptimizer/MTGOptimizer.cpp
@@ -10,8 +10,17 @@ void print_vector(std::vector<unsigned int> v) {
 int main()
 {
 	std::vector<unsigned int> deck (24, 0);
-	for (int i = 0; i < 36; i++) {
+	for (int i = 0; i < 10; i++) {
 		deck.push_back(1);
+	}
+	for (int i = 0; i < 10; i++) {
+		deck.push_back(2);
+	}
+	for (int i = 0; i < 10; i++) {
+		deck.push_back(3);
+	}
+	for (int i = 0; i < 5; i++) {
+		deck.push_back(4);
 	}
 	print_vector(deck);
 	float result = test_deck(deck, 5, 10000, false);
